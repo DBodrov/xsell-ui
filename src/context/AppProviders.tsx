e48acf7da35067a2f3221c67aa20b2eef57702a3
Бодров Дмитрий Михайлович <D.Bodrov@otpbank.ru>
2020-07-31 08:37:16 +0300

    Merge pull request #7 in DC/x-sell-ui from fix-env to master
    Squashed commit of the following:
    
    commit 8bac8426c2ea11eca9a03ad1c205a065e49960d1
    Author: D.Bodrov <D.Bodrov@otpbank.ru>
    Date:   Fri Jul 31 08:35:58 2020 +0300
    
        fix env, fix npm registry

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'emotion-theming';
-// import { ProdMode } from 'services/Environment';
+import { ProdMode } from 'services/Environment';
 import { theme } from '../globalStyles';
 import { AuthProvider } from './Auth';
 import { ErrorProvider } from './Error';
 import { TestApp } from './__mocks__/TestApp';
 
 export function AppProviders({ children }: any) {
-  if (window.location.pathname.indexOf('/dev/app') > -1) {
+  if (!ProdMode && window.location.pathname.indexOf('/dev/app') > -1) {
     return (
       <ThemeProvider theme={theme}>
         <TestApp />;
