b5e67eb3037a78bb02a131f4fedf74230a90c6c6
Бодров Дмитрий Михайлович <D.Bodrov@otpbank.ru>
2020-09-22 10:06:55 +0300

    Merge pull request #51 in DC/x-sell-ui from add_anketa_mocks to master
    Squashed commit of the following:
    
    commit 172399f835a9726ab88ec7c22b30103679e07fd3
    Author: DBodrov <dmbodrov@hotmail.com>
    Date:   Tue Sep 22 10:01:06 2020 +0300
    
        fix color textarea
    
    commit 7c94ee3c1e8ff53efc4d9c72e9901b97309c374c
    Author: DBodrov <dmbodrov@hotmail.com>
    Date:   Sun Sep 20 01:30:58 2020 +0300
    
        add anketa mocks

--- src/context/__mocks__/TestApp.tsx
+++ src/context/__mocks__/TestApp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {AllProviders} from 'utils/test-utils';
 // import {TLandingCode} from 'context/Auth';
 // import {LoginPage} from 'screens/Auth/LoginPage';
-import {PassportPage} from 'pages/Anketa/PassportPage';
+import {JobInfo} from 'pages/Anketa/JobInfo';
 
 // const landingMap: Record<string, TLandingCode> = {
 //   '1': 'LANDING_TEST_1',
@@ -14,12 +14,12 @@ import {PassportPage} from 'pages/Anketa/PassportPage';
 export function TestApp() {
   // const search = window.location.search.split('=')[1];
   const sendPassport = (step: string, anketa: any) => {
-    console.log(step, anketa);
+    //console.log(step, anketa);
   };
   // console.log(search);
   return (
-    <AllProviders anketaContext={{updateAnketa: sendPassport, step: 'PASSPORT'}}>
-      <PassportPage />
+    <AllProviders anketaContext={{updateAnketa: sendPassport, step: 'REGISTRATION_ADDRESS', anketa: {registrationAddress: 'РФ, Москва, улица Мира, 107'}}}>
+      <JobInfo />
     </AllProviders>
   );
 }
--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -5,6 +5,9 @@ export const handlers = [
   rest.post('/gateway/reject-offer', async (req, res, ctx) => {
     return res(ctx.json({code: 'OK'}));
   }),
+  rest.post('/gateway/report/store-event', async (req, res, ctx) => {
+    return res(ctx.json({code: 'OK'}));
+  }),
 
   rest.post('/gateway/auth-status', (req, res, ctx) => {
     const {cookies} = req;
@@ -110,7 +113,106 @@ export const handlers = [
   }),
   /** ANKETA HANDLERS */
   rest.post('/gateway/credit-application/get-session-app', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({status: 'LOAN_PARAMS'}));
+    //TODO: Anketa steps switch
+    return res(ctx.status(200), ctx.json({status: 'REGISTRATION_ADDRESS'}));
+  }),
+
+  rest.post('/gateway/credit-application/agree-to-sign-documents', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+
+  rest.post('/gateway/credit-application/archive-app', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+
+  rest.post('/gateway/credit-application/get-employee-monthly-payment', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({monthlyPayment: 12345}));
+  }),
+  rest.post('/gateway/credit-application/get-monthly-payment', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({monthlyPayment: 12345}));
+  }),
+  rest.post('/gateway/credit-application/send-documents', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/send-signature-code', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/sign-agreement', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        passwordLength: 4,
+        passwordLifetimeInSeconds: 60,
+        retryTimeInSeconds: 0,
+      }),
+    );
+  }),
+  rest.post('/gateway/credit-application/submit-form', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/update-session-app-account-transfer-details', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/update-session-app-card-transfer-details', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post(
+    '/gateway/credit-application/update-session-app-confirm-upload-passport-photo',
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({code: 'OK'}));
+    },
+  ),
+  rest.post('/gateway/credit-application/update-session-app-details', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/update-session-app-loan-params', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/update-session-app-passport', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post(
+    '/gateway/credit-application/update-session-app-refuse-upload-passport-photo',
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({code: 'OK'}));
+    },
+  ),
+  rest.post('/gateway/credit-application/update-session-app-registration-address', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/upload-passport-photo', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/credit-application/verify-agreement-signature', (req, res, ctx) => {
+    if (req.body['verificationCode'] === '0000') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          verified: false,
+        }),
+      );
+    }
+    return res(ctx.status(200), ctx.json({verified: true}));
+  }),
+  rest.post('/gateway/credit-application/verify-signature-code', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({code: 'OK'}));
+  }),
+  rest.post('/gateway/customer-profile/get-work-experience', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({workExperienceMonths: 18}));
+  }),
+  rest.post('/gateway/customer-profile/get-otp-cards', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        customerOtpCards: [
+          {
+            id: '1',
+            number: '1234567890123456',
+            expirationDate: '09/23',
+          },
+        ],
+      }),
+    );
   }),
 ];
 
