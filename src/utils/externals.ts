2df0b17798a7d22924a718b078beeca78338c9e4
D.Bodrov <D.Bodrov@otpbank.ru>
2021-03-29 12:19:05 +0300

    update all

--- src/utils/externals.ts
+++ src/utils/externals.ts
@@ -1,6 +1,7 @@
 export const OPROSSO_FORM = 'https://oprosso.net/p/9tCpB2ETou5QY2FxM';
 export const OTP_INN = '7708001614';
-export const HERO_IMG_URL = 'https://cash.otpbank.ru/public/images/girl3a.png'
+export const HERO_IMG_URL = 'https://cash.otpbank.ru/public/images/girl3a.png';
+export const COMMON_RULES='https://cash.otpbank.ru/public/OTP_Common_rules_01032021.pdf';
 
 export const OPROSSO = {
   LANDING: 'http://opros.so/DtxQ',
